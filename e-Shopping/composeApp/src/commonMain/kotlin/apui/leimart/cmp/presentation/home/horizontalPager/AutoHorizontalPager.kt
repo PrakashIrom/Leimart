fb9fca373355a31def73e83d68dc0c5914ce891d
prakash <apuiirom@gmailcom>
2025-05-21 00:39:40 +0530

    minor ui change

--- e-Shopping/composeApp/src/commonMain/kotlin/apui/leimart/cmp/presentation/home/horizontalPager/AutoHorizontalPager.kt
+++ e-Shopping/composeApp/src/commonMain/kotlin/apui/leimart/cmp/presentation/home/horizontalPager/AutoHorizontalPager.kt
@@ -2,36 +2,28 @@ package apui.leimart.cmp.presentation.home.horizontalPager
 
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsDraggedAsState
 import androidx.compose.foundation.interaction.collectIsPressedAsState
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.snapshotFlow
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import e_shopping.composeapp.generated.resources.Res
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collectLatest
-import org.jetbrains.compose.resources.DrawableResource
-import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun AutoHorizontalPager(
@@ -82,14 +74,14 @@ fun HorizontalPagerItem(
 ) {
     Card(
         modifier = modifier.padding(start = 15.dp, top = 15.dp, end = 15.dp, bottom = 5.dp)
-            .size(height = 200.dp, width = 300.dp),
+            .size(height = 200.dp, width = 100.dp),
         shape = RoundedCornerShape(10.dp)
     ) {
         AsyncImage(
             model = Res.getUri(pageItems[page]),
             contentDescription = "Image",
             contentScale = ContentScale.FillBounds,
-            modifier = modifier.height(40.dp).fillMaxWidth().clickable(
+            modifier = Modifier.clickable(
                 indication = LocalIndication.current,
                 interactionSource = pageInteractionSource
             ) {}
